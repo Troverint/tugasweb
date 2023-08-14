32b2b10f6df6b5cd0f7a5c22af43c03e1573016d
Troverint <118240279+Troverint@users.noreply.github.com>
2023-08-14 00:47:08 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
+import Main from './components/Main';
+
+
+function App() {
+  return (
+    <div>
+     <Navbar />
+     <Main/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
